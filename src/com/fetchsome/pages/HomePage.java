7a206260cc8baafc2415663f106da90c681fa2e5
dincim <dincmurad@gmail.com>
2018-03-10 11:14:54 -0800

    fetchsome homepage test is added

---
+++ src/com/fetchsome/pages/HomePage.java
@@ -0,0 +1,33 @@
+package com.fetchsome.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HomePage {
+	
+
+	WebDriver driver = new ChromeDriver();
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", 
+				"/Users/muratdinc/Documents/Libraries/drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://192.168.0.248:8080/product-search/find");
+		
+	}
+	
+	@Test
+	public void loginTest() {
+		
+		driver.findElement(By.className("auth0-lock-input")).sendKeys("murat.dinc@fairbit.com");
+		
+	}
+		
+		
+		
+	}
+	
+	
